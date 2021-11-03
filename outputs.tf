8505e6237a1b757c18bb729e1194b2f25d83a77b
Sriharsha Kalluru <skalluru@mac.local>
2021-11-03 08:35:39 +0530

    Update terraform/

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,15 @@
-output "abc" {
-  value = "Hello\nWorld"
+#output "abc" {
+#  value = "Hello\nWorld"
+#}
+#
+#output "abc1" {
+#  value = "hello"
+#}
+
+variable "abc" {
+  default = "100"
 }
 
-output "abc1" {
-  value = "hello"
+output "abc" {
+  value = var.abc
 }
