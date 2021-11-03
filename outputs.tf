91588b83fe325fae82861cf71f9355040b4f10f6
Sriharsha Kalluru <skalluru@mac.local>
2021-11-03 08:46:06 +0530

    Update terraform/

--- outputs.tf
+++ outputs.tf
@@ -14,8 +14,52 @@ output "abc" {
   value = var.abc
 }
 
-variable "abc1" {}
+variable "abc1" {
+  default = 200
+}
 
 output "abc1" {
-  value = var.abc1
+  value = "Value of abc1 = ${var.abc1}"
 }
+
+# String Data type
+variable "sample1" {
+  default = "Hello World"
+}
+
+# Number data type
+variable "sample2" {
+  default = 100
+}
+
+# Boolean Data type
+variable "sample3" {
+  default = true
+}
+
+
+variable "samplex" {
+  default = [
+    "Hello",
+    1000,
+    true,
+    "World"
+  ]
+}
+
+variable "sampley" {
+  default = {
+    string  = "Hello",
+    number  = 100,
+    boolean = true
+  }
+}
+
+output "samplex" {
+  value = "Value 1 = ${var.samplex[0]}, Value 3 = ${var.samplex[2]}"
+}
+
+output "sampley" {
+  value = "String = ${var.sampley["string"]}, Number = ${var.sampley["number"]}, Boolean = ${var.sampley["boolean"]}"
+}
+
