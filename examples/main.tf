98228f50e73fb83c6ade1b860cf4b558f9189b71
Sriharsha Kalluru <skalluru@mac.local>
2021-11-04 08:31:15 +0530

    Update terraform/

--- examples/main.tf
+++ examples/main.tf
@@ -37,6 +37,9 @@ resource "aws_security_group" "allow_sample" {
       protocol         = "-1"
       cidr_blocks      = ["0.0.0.0/0"]
       ipv6_cidr_blocks = ["::/0"]
+      prefix_list_ids  = []
+      security_groups  = []
+      self             = false
     }
   ]
 
